62b2cc9e7ce41acea8cbaf7770dd74789a592b8a
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-09 19:02:03 -0300

    Resoluçao do requisito 5

--- src/App.js
+++ src/App.js
@@ -19,26 +19,39 @@ class App extends React.Component {
     };
     this.onInputChange = this.onInputChange.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.validationButton = this.validationButton.bind(this);
+  }
+
+  onSaveButtonClick(event) {
+    event.preventDefault();
   }
 
   onInputChange({ target }) {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
     this.setState({ [name]: value }, () => {
-      const {
-        cardName, cardDescription, cardImage, cardRare,
-      } = this.state;
-      if (cardName === '' || cardDescription === ''
-      || cardImage === '' || cardRare === '') {
-        this.setState({ isSaveButtonDisabled: true });
-      } else {
-        this.setState({ isSaveButtonDisabled: false });
-      }
+      this.validationButton();
     });
   }
 
-  onSaveButtonClick(event) {
-    event.preventDefault();
+  validationButton() {
+    const {
+      cardName, cardDescription, cardImage, cardRare, cardAttr1, cardAttr2, cardAttr3,
+    } = this.state;
+    const sumAttr = parseInt(cardAttr1, 10) + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr3, 10);
+    const maxValue = 210;
+    const maxValueForAttr = 90;
+    const minValueForAttr = 0;
+    if (cardName === '' || cardDescription === ''
+    || cardImage === '' || cardRare === '' || sumAttr > maxValue
+    || cardAttr1 > maxValueForAttr || cardAttr2 > maxValueForAttr
+    || cardAttr3 > maxValueForAttr || cardAttr1 < minValueForAttr
+    || cardAttr2 < minValueForAttr || cardAttr3 < minValueForAttr
+    || cardAttr1 === '' || cardAttr2 === '' || cardAttr3 === '') {
+      return this.setState({ isSaveButtonDisabled: true });
+    }
+    return this.setState({ isSaveButtonDisabled: false });
   }
 
   render() {
