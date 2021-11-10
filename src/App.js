2c213dfd343d25ada45666c18e03e749453c3c23
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-10 13:59:35 -0300

    Resoluçao do requisito 8

--- src/App.js
+++ src/App.js
@@ -141,9 +141,26 @@ class App extends React.Component {
               isSaveButtonDisabled={ isSaveButtonDisabled }
               onInputChange={ onInputChange }
               onSaveButtonClick={ onSaveButtonClick }
-              arrayCards={ arrayCards }
             />
           </div>
+        </div>
+        <div>
+          {
+            arrayCards.map((card) => (
+              <div key={ card.cardName }>
+                <Card
+                  cardName={ card.cardName }
+                  cardDescription={ card.cardDescription }
+                  cardAttr1={ card.cardAttr1 }
+                  cardAttr2={ card.cardAttr2 }
+                  cardAttr3={ card.cardAttr3 }
+                  cardImage={ card.cardImage }
+                  cardRare={ card.cardRare }
+                  cardTrunfo={ card.cardTrunfo }
+                />
+              </div>
+            ))
+          }
         </div>
       </div>
     );
