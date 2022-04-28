762cb27c182b6dbedcf6ed7e60f017bc5734b06a
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-28 10:44:12 -0300

    fix: correção e inserção de novas tags;

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,9 +10,9 @@ class Form extends React.Component {
     } = this.props;
 
     return (
-      <div>
-        <label htmlFor="cardName">
-          Nome da carta:
+      <div className="form">
+        <label htmlFor="cardName" className="name-input">
+          <p>Nome:</p>
           <input
             type="text"
             name="cardName"
@@ -21,86 +21,92 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label htmlFor="cardDescription">
-          Descrição da carta:
+        <label htmlFor="cardDescription" className="description-label">
+          <p>Descrição</p>
           <textarea
             name="cardDescription"
             data-testid="description-input"
             cols="30"
-            rows="10"
+            rows="4"
+            maxLength="115"
             value={ cardDescription }
             onChange={ onInputChange }
           >
             { cardDescription }
           </textarea>
         </label>
-        <label htmlFor="cardAttr1">
-          Atributo 1:
-          <input
-            min="0"
-            max="90"
-            type="number"
-            name="cardAttr1"
-            data-testid="attr1-input"
-            value={ cardAttr1 }
-            onChange={ onInputChange }
-          />
-        </label>
-        <label htmlFor="cardAttr2">
-          Atributo 2:
-          <input
-            min="0"
-            max="90"
-            type="number"
-            name="cardAttr2"
-            data-testid="attr2-input"
-            value={ cardAttr2 }
-            onChange={ onInputChange }
-          />
-        </label>
-        <label htmlFor="cardAttr3">
-          Atributo 3:
-          <input
-            min="0"
-            max="90"
-            type="number"
-            name="cardAttr3"
-            data-testid="attr3-input"
-            value={ cardAttr3 }
-            onChange={ onInputChange }
-          />
-        </label>
-        <label htmlFor="cardImage">
-          Insira a URL da imagem:
-          <input
-            type="text"
-            name="cardImage"
-            data-testid="image-input"
-            value={ cardImage }
-            onChange={ onInputChange }
-          />
-        </label>
-        <label htmlFor="cardRare">
-          Selecione a Raridade:
-          <select
-            name="cardRare"
-            data-testid="rare-input"
-            value={ cardRare }
-            onChange={ onInputChange }
-          >
-            <option value="normal">normal</option>
-            <option value="raro">raro</option>
-            <option value="muito raro">muito raro</option>
-          </select>
-        </label>
-        <label htmlFor="cardTrunfo">
-          { hasTrunfo === false ? <Checkbox
-            cardTrunfo={ cardTrunfo }
-            hasTrunfo={ hasTrunfo }
-            onInputChange={ onInputChange }
-          />
-            : <span>Você já tem um Super Trunfo em seu baralho</span> }
-        </label>
+        <div className="attributes">
+          <label htmlFor="cardAttr1">
+            <span>Atributo 1:</span>
+            <input
+              min="0"
+              max="90"
+              type="number"
+              name="cardAttr1"
+              data-testid="attr1-input"
+              value={ cardAttr1 }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="cardAttr2">
+            <span>Atributo 2:</span>
+            <input
+              min="0"
+              max="90"
+              type="number"
+              name="cardAttr2"
+              data-testid="attr2-input"
+              value={ cardAttr2 }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="cardAttr3">
+            <span>Atributo 3:</span>
+            <input
+              min="0"
+              max="90"
+              type="number"
+              name="cardAttr3"
+              data-testid="attr3-input"
+              value={ cardAttr3 }
+              onChange={ onInputChange }
+            />
+          </label>
+        </div>
+        <div className="attributes-2">
+          <label htmlFor="cardImage">
+            <p>Insira a URL da imagem:</p>
+            <input
+              id="url"
+              type="text"
+              name="cardImage"
+              data-testid="image-input"
+              value={ cardImage }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="cardRare">
+            <p>Selecione a Raridade:</p>
+            <select
+              name="cardRare"
+              data-testid="rare-input"
+              value={ cardRare }
+              onChange={ onInputChange }
+            >
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+          <label htmlFor="cardTrunfo">
+            { hasTrunfo === false ? <Checkbox
+              cardTrunfo={ cardTrunfo }
+              hasTrunfo={ hasTrunfo }
+              onInputChange={ onInputChange }
+            />
+              : <span>Você já tem um Super Trunfo em seu baralho</span> }
+          </label>
+        </div>
         <button
           type="submit"
           data-testid="save-button"
